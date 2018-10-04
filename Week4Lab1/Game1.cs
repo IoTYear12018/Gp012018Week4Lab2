cc502aca058b23c6c65b8be0f17858a42babf46a
ppowell <ppowell@LAB-D2039-32>
2018-10-04 11:56:51 +0100

    Add project files.

---
+++ Week4Lab1/Game1.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Sprites;
+
+namespace Week4Lab1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Viewport mapViewport;
+        Viewport originalViewPort;
+        private Texture2D _txCharacter;
+        private SimpleSprite CharacterSprite;
+        private Texture2D _txBackGround;
+        private SimpleSprite BackGroundSprite;
+        Vector2 CharacterPosition = new Vector2(10, 10);
+        private Texture2D _txDot;
+        private SimpleSprite DotSprite;
+        private SpriteFont GameFont;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            // Sample the original Viewport
+            originalViewPort = GraphicsDevice.Viewport;
+            GraphicsDevice.Viewport = originalViewPort;
+            // Create the map viewport
+            mapViewport.Bounds = new Rectangle(0, 0,
+                originalViewPort.Bounds.Width / 10,
+                originalViewPort.Bounds.Height / 10);
+            mapViewport.X = 0;
+            mapViewport.Y = 0;
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            _txBackGround = Content.Load<Texture2D>(@"Textures\backgroundImage");
+            BackGroundSprite = new SimpleSprite(_txBackGround, Vector2.Zero);
+            _txCharacter = Content.Load<Texture2D>(@"Textures\body2");
+            CharacterSprite = new SimpleSprite(_txCharacter, originalViewPort.Bounds.Center.ToVector2());
+            _txDot = Content.Load<Texture2D>(@"Textures\body");
+            DotSprite = new SimpleSprite(_txDot, mapViewport.Bounds.Center.ToVector2());
+            GameFont = Content.Load<SpriteFont>(@"GameFont");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            float speed = 5.0f;
+            float scale = 0.01f;
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            Vector2 perviousPos = CharacterSprite.Position;
+            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                CharacterSprite.Move(new Vector2(-1, 0) * speed);
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+                CharacterSprite.Move(new Vector2(1, 0) * speed);
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                CharacterSprite.Move(new Vector2(0, -1) * speed);
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+                CharacterSprite.Move(new Vector2(0, 1) * speed);
+            if (!GraphicsDevice.Viewport.Bounds
+                .Contains(CharacterSprite.BoundingRect))
+                CharacterSprite.Move(perviousPos - CharacterSprite.Position);
+            DotSprite.Position = CharacterPosition * scale;
+            DotSprite.text = DotSprite.Position.ToString();
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            CharacterPosition = CharacterSprite.Position;
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            BackGroundSprite.draw(spriteBatch,originalViewPort.Bounds);
+            CharacterSprite.text = CharacterSprite.Position.ToString();
+            CharacterSprite.draw(spriteBatch, GameFont);
+            CharacterSprite.draw(spriteBatch);
+            //spriteBatch.Draw(_txBackGround, originalViewPort.Bounds, Color.White);
+
+            //spriteBatch.Draw(_txCharacter, CharacterPosition, Color.White);
+            spriteBatch.End();
+            
+            GraphicsDevice.Viewport = mapViewport;
+            spriteBatch.Begin();
+            DotSprite.draw(spriteBatch, 0.1f);
+            BackGroundSprite.draw(spriteBatch,mapViewport.Bounds);
+            DotSprite.text = DotSprite.Position.ToString();
+            DotSprite.draw(spriteBatch, GameFont);
+            //spriteBatch.Draw(_txDot, CharacterPosition * 0.1f,
+            //                null, Color.White, 0f, Vector2.Zero, 0.1f, SpriteEffects.None, 0);
+            //spriteBatch.Draw(_txBackGround, mapViewport.Bounds, Color.White);
+            spriteBatch.End();
+            // TODO: Add your drawing code here
+            GraphicsDevice.Viewport = originalViewPort;
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Week4Lab1/SimpleSprite.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Sprites
+{
+    public class SimpleSprite
+    {
+        public Texture2D Image;
+        public Vector2 Position;
+        public Rectangle BoundingRect;
+        public bool Visible = true;
+        public string text = "Here I am";
+
+        public SimpleSprite(Texture2D spriteImage,
+                            Vector2 startPosition)
+        {
+            Image = spriteImage;
+            Position = startPosition;
+            BoundingRect = new Rectangle((int)startPosition.X, (int)startPosition.Y, Image.Width, Image.Height);
+
+        }
+
+        public bool InCollision(SimpleSprite other)
+        {
+            return BoundingRect.Intersects(other.BoundingRect);
+        }
+
+        public void draw(SpriteBatch sp)
+        {
+            if(Visible)
+                sp.Draw(Image, Position, Color.White);
+        }
+        public void draw(SpriteBatch sp,SpriteFont font)
+        {
+            if (Visible)
+            {
+                Vector2 textSize = font.MeasureString(text);
+                int ypos = (int)Position.Y - 20;
+                int xpos = (int)BoundingRect.Center.ToVector2().X - (int)textSize.X / 2;
+                sp.Draw(Image, Position, Color.White);
+                sp.DrawString(font, text, new Vector2(xpos,ypos), Color.White);
+            }
+
+        }
+        public void draw(SpriteBatch sp, float scale)
+        {
+            if (Visible)
+                sp.Draw(Image, Position * scale,
+                            null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0);
+        }
+
+        public void draw(SpriteBatch sp, Rectangle bound)
+        {
+            if (Visible)
+                sp.Draw(Image, bound, Color.White);
+        }
+        public void Move(Vector2 delta)
+        {
+            Position += delta;
+            BoundingRect = new Rectangle((int)Position.X, 
+                (int)Position.Y, Image.Width, Image.Height);
+            BoundingRect.X = (int)Position.X;
+            BoundingRect.Y = (int)Position.Y;
+        }
+    }
+}
